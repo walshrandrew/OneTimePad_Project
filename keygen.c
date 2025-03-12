ef95631ac10a670c9ce886c7f6665a9d5c6457aa
Andrew Walsh <154630935+walshrandrew@users.noreply.github.com>
2025-03-12 15:53:27 -0600

    Update keygen.c

--- keygen.c
+++ keygen.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//keygen keylength
+//second argument argv([1]) is length of the key to generate
+//keygen does not access files or connect to network socket
 
 int main(int agrc, char* argv[])
 {
