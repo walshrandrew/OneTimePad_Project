3699ec11248c193fb329419319fe53ddea8c99cb
Andrew Walsh <154630935+walshrandrew@users.noreply.github.com>
2025-03-12 23:35:30 +0000

    I think it is done. Not sure how to debug it.

--- keygen.c
+++ keygen.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int agrc, char* argv[])
 {
-    if(agrc > 2)
+    long keylength = strtol(argv[1], NULL, 10);
+    int key; 
+    char encrypted_key[keylength +1];
+    srand(time(0)); //seed random number
+    char valid[28] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    if(agrc != 2 || keylength <= 0)
     {
-        fprintf(stderr, "Error: argc over two\n");
+        fprintf(stderr, "Error: argv incorrect\n");
+        exit(1);
     }
     else
     {
-        int keylength = argv[1];
-    }
-}
-
-// can I add things and git is happy now?
+        for(int i = 0; i < keylength; i++)
+        {
+            key = rand() % 27;
+            encrypted_key[i] = valid[key];
+        } 
+        encrypted_key[keylength] = '\0';
+        fprintf(stdout, "%s\n", encrypted_key);
+    } return 0;
+} 
