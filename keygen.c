a99fccfc9ccbcbfd601197a2e5f99384b4d44a40
Andrew Walsh <154630935+walshrandrew@users.noreply.github.com>
2025-03-12 22:12:16 +0000

    poo poo

--- keygen.c
+++ keygen.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(int agrc, char* argv[])
+{
+    if(agrc > 2)
+    {
+        fprintf(stderr, "Error: argc over two\n");
+    }
+    else
+    {
+        int keylength = argv[1];
+    }
+}
